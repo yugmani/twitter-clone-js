00e045a217e60c2429229dacf9f6e2ec0d922516
Himalaya <yugmani@yahoo.com>
2020-10-12 15:34:01 -0700

    finalize:front-end

--- README.md
+++ README.md
@@ -58,32 +58,6 @@ It needs to follow a class structure like this:
 
     List Item: padding-left: 20px; padding-top and bottom: 8px
 
-## TODO(API): Set up NodeJS Server
-
-Return `Hello World` for the root `/` get request
-
-[Example](https://expressjs.com/en/starter/hello-world.html)
-
-## TODO(API, TOGETHER): Create API endpoint `/tweeets` to return a list of tweets based on query
-
-Use [axios](https://github.com/axios/axios) for making an API request to Twitter API
-
-Console log the data
-
-Return as a response
-
-## TODO(API): Create Twitter `get()` helper function to move the Twitter API logic
-
-#### HINTS:
-
-    - Create Twitter class inside `api/helpers/twitter.js`
-    - Create a `get()` function that takes in the necessary parameters
-    - Inside `get()` return `axios.get(...)`
-    - Import Twitter class in `app.js` with `const twitter = new Twitter();`
-    - Initialize and use the `twitter` object to now do somethong like `twitter.get(...).then(...).catch(...)`
-
-## TODO(API, TOGETHER): Move the API Token to .env file and import it
-
 ## TODO: Complete `getTwitterData()` function to retrieve data from our API
 
 For now, I want you to use the following static url to get data from api:
@@ -166,3 +140,59 @@ This time you are building a dynamic url that will change based on the user's se
 ## TODO: Clean Up
 
 ## WE ARE DONE!
+
+# twitter-clone-js
+
+## Goals: Create a twitter clone to let users search for tweets and view them.
+
+## User Stories
+
+**TASK: Complete User story for trending hashtags and next page functionality**
+
+- As a user, I want to be able to input my serch so that I can search for tweets.
+- As a user, I want to be able to see the tweets I searched for so that I can view them.
+- As a user, I want to be able to view the text, photo, gifs or videos of the tweet so that I can view all of them contents of the tweet.
+
+* As a user, I want to see the trending hashtags currently on twitter so that I can later use them to search for tweets.
+* As a user, I want to click on the trending hashtags so that I can see the tweets for that hashtag.
+* As a user, I want to click next page so that I can see the next page of the tweets.
+
+## Flow Diagram(Swimlane)
+
+**TASK: Complete Swimlane for next page click**
+:arrow_forward:[twitter-clone-swimline-Flow](./assets/images/twitter-clone-swimline-flow)
+
+## Business Requirements
+
+**TASK: Complete Business Requirements for Home Page**
+
+1. Pages:
+
+- Home Page
+
+2. Home Page
+
+- Allow a user to type in and search for tweets based on what they type
+- Show the list of tweets based on search
+  _Show text_
+  _Show images if available_
+  _Show video if available_
+  _Show gif if available_
+  _Show how long ago this tweet was created_
+  _Show the user's full name who created the tweet_
+  _Show the profile of the user_
+  _Show the twitter handle of the user who created this tweet_
+
+* Show 10 tweets only at the start
+* Allow a user to get the next 10 tweets or the next page of the tweets.
+* Show trending hashtags in a list
+* Allow a user to click on the trending hashtag and search for it
+* Show the profile photo of the person using the app
+
+## Technical Requirements
+
+- Create a Developer Account for Twitter
+- Make sure to only show the next page link when there are no more results for next page
+- Use the attribute “next_results”
+- Use moment.js for showing the date of the tweet from the current date
+- Use the documentation here for getting Search Tweets
--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
 const URL = "http://localhost:3000/tweets";
  // const url = 'http://localhost:3000/tweets?q=coding&count=10';
 
+let nextPageUrl = null;
+
+const onEnter = (e)=>{
+    if(e.key == "Enter"){
+        getTwitterData();
+    }
+}
+
+const onNextPage = ()=>{
+   if(nextPageUrl){
+       getTwitterData(true);
+   }
+}
+
 /**
  * Retrive Twitter Data from API
  */
-
-
-const getTwitterData = () => {
+const getTwitterData = (nextPage=false) => {
     const query = document.getElementById("user-search-input").value;
     const encodedQuery = encodeURIComponent(query);
    if(query){
-        const fullUrl = `${URL}?q=${encodedQuery}&count=10`;
+        let fullUrl = `${URL}?q=${encodedQuery}&count=10`;
+        if(nextPage && nextPageUrl){
+            fullUrl = nextPageUrl;
+        }
         fetch(fullUrl).then((response)=>{
             return response.json();
         }).then((data)=>{
-            console.log(data);
-            buildTweets(data.statuses);
+            // console.log(data);
+            buildTweets(data.statuses, nextPage);
+            saveNextPage(data.search_metadata);
+            nextPageButtonVisibility(data.search_metadata);
         })
     } else {
     return;
@@ -25,64 +42,83 @@ const getTwitterData = () => {
 const searchButton = document.getElementById("searchIcon");
 searchButton.addEventListener("click", getTwitterData);
 
-getTwitterData();
 
 /**
  * Save the next page data
  */
 const saveNextPage = (metadata) => {
+    if(metadata.next_results){
+        nextPageUrl = `${URL}${metadata.next_results}`;
+    } else {
+        nextPageUrl = null;
+    }
 }
 
 /**
  * Handle when a user clicks on a trend
  */
 const selectTrend = (e) => {
+   const text = e.innerText;
+   document.getElementById("user-search-input").value = text; 
+    getTwitterData();
 }
 
 /**
  * Set the visibility of next page based on if there is data on next page
  */
 const nextPageButtonVisibility = (metadata) => {
+    if(metadata.next_results){
+       document.getElementById('next-page').style.visibility = "visible";
+    } else {
+        document.getElementById('next-page').style.visibility = "hidden";
+    }
+
 }
 
 /**
  * Build Tweets HTML based on Data from API
  */
 const buildTweets = (tweets, nextPage) => {
-let twitterContent = "";
-tweets.map((tweet) =>{
-    const created = moment(tweet.created_at);
-    const createdDate = moment(created).fromNow();
-    twitterContent += `
-        <div class="tweet-container">
-            <div class="tweet-user-info">
-                <div class="tweet-user-profile" style="background-image:url(${tweet.user.profile_image_url_https})"></div>
-                <div class="tweet-user-name-container">
-                    <div class="tweet-user-fullname">
-                        ${tweet.user.name}
+    let twitterContent = "";
+    tweets.map((tweet) =>{
+        const created = moment(tweet.created_at);
+        const createdDate = moment(created).fromNow();
+        twitterContent += `
+            <div class="tweet-container">
+                <div class="tweet-user-info">
+                    <div class="tweet-user-profile" style="background-image:url(${tweet.user.profile_image_url_https})"></div>
+                    <div class="tweet-user-name-container">
+                        <div class="tweet-user-fullname">
+                            ${tweet.user.name}
+                        </div>
+                        <div class="tweet-user-username">
+                            @${tweet.user.screen_name}
+                        </div>
                     </div>
-                    <div class="tweet-user-username">
-                        @${tweet.user.screen_name}
-                    </div>
+                </div>
+                `
+                if(tweet.extended_entities && tweet.extended_entities.media.length>0){
+                    twitterContent += buildImages(tweet.extended_entities.media);
+                    twitterContent += buildVideo(tweet.extended_entities.media);
+                }
+                twitterContent +=`
+                <div class="tweet-text-container">
+                    ${tweet.full_text}
+                </div>
+                <div class="tweet-date-container">
+                    ${createdDate}
                 </div>
             </div>
             `
-            if(tweet.extended_entities && tweet.extended_entities.media.length>0){
-                twitterContent += buildImages(tweet.extended_entities.media);
-                twitterContent += buildVideo(tweet.extended_entities.media);
-            }
-            twitterContent +=`
-            <div class="tweet-text-container">
-                ${tweet.full_text}
-            </div>
-            <div class="tweet-date-container">
-                ${createdDate}
-            </div>
-        </div>
-        `
-})
- document.querySelector('.tweets-list').innerHTML = twitterContent;
+    })
 
+    if(nextPage){
+        document.querySelector('.tweets-list').insertAdjacentHTML('beforeend', twitterContent);
+        document.querySelector('.tweets-welcome-message').style.visibility = "hidden";
+    } else {
+    document.querySelector('.tweets-list').innerHTML = twitterContent;
+    document.querySelector('.tweets-welcome-message').style.visibility = "hidden";
+    }
 }
 
 /**
@@ -112,9 +148,10 @@ const buildVideo = (mediaList) => {
     mediaList.map((media)=>{
         if(media.type=='video'){
             videoExists = true;
+            const videoVariant = media.video_info.variants.find((variant)=>variant.content_type == 'video/mp4');
             videoContent += ` 
                     <video controls>
-                        <source src="${media.video_info.variants[0].url}" type="video/mp4">
+                        <source src="${videoVariant.url}" type="video/mp4">
                     </video>
                     `  
         } else if (media.type == 'animated_gif'){
